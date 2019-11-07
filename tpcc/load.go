661e53ef4014203f607147d8f2cbfd5177bec6f7
siddontang <siddontang@gmail.com>
2019-11-07 09:51:35 +0800

    add customer init
    Signed-off-by: siddontang <siddontang@gmail.com>

--- tpcc/load.go
+++ tpcc/load.go
@@ -3,6 +3,7 @@ package tpcc
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/siddontang/go-tpc/pkg/load"
 )
@@ -28,12 +29,12 @@ func (w *Workloader) loadItem(ctx context.Context, tableID int) error {
 
 	for i := 0; i < maxItems; i++ {
 		s.Buf.Reset()
-		i_im_id := randInt(s.R, 1, 10000)
-		i_price := float64(randInt(s.R, 100, 10000)) / float64(100.0)
-		i_name := randChars(s.R, s.Buf, 14, 24)
-		i_data := randOriginalString(s.R, s.Buf)
+		iImID := randInt(s.R, 1, 10000)
+		iPrice := float64(randInt(s.R, 100, 10000)) / float64(100.0)
+		iName := randChars(s.R, s.Buf, 14, 24)
+		iData := randOriginalString(s.R, s.Buf)
 
-		v := fmt.Sprintf(`(%d, %d, '%s', %f, '%s')`, i+1, i_im_id, i_name, i_price, i_data)
+		v := fmt.Sprintf(`(%d, %d, '%s', %f, '%s')`, i+1, iImID, iName, iPrice, iData)
 
 		if err := l.InsertValue(ctx, v); err != nil {
 			return err
@@ -48,17 +49,17 @@ func (w *Workloader) loadWarhouse(ctx context.Context, tableID int, warehouse in
 	hint := fmt.Sprintf("INSERT INTO warehouse%d (w_id, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd) VALUES ", tableID)
 	l := load.NewBatchLoader(s.Conn, hint)
 
-	w_name := randChars(s.R, s.Buf, 6, 10)
-	w_stree1 := randChars(s.R, s.Buf, 10, 20)
-	w_stree2 := randChars(s.R, s.Buf, 10, 20)
-	w_city := randChars(s.R, s.Buf, 10, 20)
-	w_state := randState(s.R, s.Buf)
-	w_zip := randZip(s.R, s.Buf)
-	w_tax := randTax(s.R)
-	w_ytd := 300000.00
+	wName := randChars(s.R, s.Buf, 6, 10)
+	wStree1 := randChars(s.R, s.Buf, 10, 20)
+	wStree2 := randChars(s.R, s.Buf, 10, 20)
+	wCity := randChars(s.R, s.Buf, 10, 20)
+	wState := randState(s.R, s.Buf)
+	wZip := randZip(s.R, s.Buf)
+	wTax := randTax(s.R)
+	wYtd := 300000.00
 
 	v := fmt.Sprintf(`(%d, '%s', '%s', '%s', '%s', '%s', '%s', %f, %f)`,
-		warehouse, w_name, w_stree1, w_stree2, w_city, w_state, w_zip, w_tax, w_ytd)
+		warehouse, wName, wStree1, wStree2, wCity, wState, wZip, wTax, wYtd)
 	l.InsertValue(ctx, v)
 
 	return l.Flush(ctx)
@@ -75,27 +76,26 @@ s_dist_07, s_dist_08, s_dist_09, s_dist_10, s_ytd, s_order_cnt, s_remote_cnt, s_
 
 	for i := 0; i < stockPerWarehouse; i++ {
 		s.Buf.Reset()
-		s_i_id := i + 1
-		s_w_id := warehouse
-		s_quantity := randInt(s.R, 10, 100)
-		s_dist_01 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_02 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_03 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_04 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_05 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_06 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_07 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_08 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_09 := randLetters(s.R, s.Buf, 24, 24)
-		s_dist_10 := randLetters(s.R, s.Buf, 24, 24)
-		s_ytd := 0
-		s_order_cnt := 0
-		s_remote_cnt := 0
-		s_data := randOriginalString(s.R, s.Buf)
+		sIID := i + 1
+		sWID := warehouse
+		sQuantity := randInt(s.R, 10, 100)
+		sDist01 := randLetters(s.R, s.Buf, 24, 24)
+		sDist02 := randLetters(s.R, s.Buf, 24, 24)
+		sDist03 := randLetters(s.R, s.Buf, 24, 24)
+		sDist04 := randLetters(s.R, s.Buf, 24, 24)
+		sDist05 := randLetters(s.R, s.Buf, 24, 24)
+		sDist06 := randLetters(s.R, s.Buf, 24, 24)
+		sDist07 := randLetters(s.R, s.Buf, 24, 24)
+		sDist08 := randLetters(s.R, s.Buf, 24, 24)
+		sDist09 := randLetters(s.R, s.Buf, 24, 24)
+		sDist10 := randLetters(s.R, s.Buf, 24, 24)
+		sYtd := 0
+		sOrderCnt := 0
+		sRemoteCnt := 0
+		sData := randOriginalString(s.R, s.Buf)
 
 		v := fmt.Sprintf(`(%d, %d, %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d, %d, '%s')`,
-			s_i_id, s_w_id, s_quantity, s_dist_01, s_dist_02, s_dist_03, s_dist_04, s_dist_05, s_dist_06, s_dist_07,
-			s_dist_08, s_dist_09, s_dist_10, s_ytd, s_order_cnt, s_remote_cnt, s_data)
+			sIID, sWID, sQuantity, sDist01, sDist02, sDist03, sDist04, sDist05, sDist06, sDist07, sDist08, sDist09, sDist10, sYtd, sOrderCnt, sRemoteCnt, sData)
 		if err := l.InsertValue(ctx, v); err != nil {
 			return err
 		}
@@ -112,20 +112,20 @@ d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id) VALUES `, tableID)
 	l := load.NewBatchLoader(s.Conn, hint)
 
 	for i := 0; i < districtPerWarehouse; i++ {
-		d_id := i + 1
-		d_w_id := warehouse
-		d_name := randChars(s.R, s.Buf, 6, 10)
-		d_street1 := randChars(s.R, s.Buf, 10, 20)
-		d_street2 := randChars(s.R, s.Buf, 10, 20)
-		d_city := randChars(s.R, s.Buf, 10, 20)
-		d_state := randState(s.R, s.Buf)
-		d_zip := randZip(s.R, s.Buf)
-		d_tax := randTax(s.R)
-		d_ytd := 300000.00
-		d_next_o_id := 3001
+		dID := i + 1
+		dWID := warehouse
+		dName := randChars(s.R, s.Buf, 6, 10)
+		dStreet1 := randChars(s.R, s.Buf, 10, 20)
+		dStreet2 := randChars(s.R, s.Buf, 10, 20)
+		dCity := randChars(s.R, s.Buf, 10, 20)
+		dState := randState(s.R, s.Buf)
+		dZip := randZip(s.R, s.Buf)
+		dTax := randTax(s.R)
+		dYtd := 300000.00
+		dNextOID := 3001
 
-		v := fmt.Sprintf(`(%d, %d, '%s', '%s', '%s', '%s', '%s', '%s', %f, %f, %d)`, d_id, d_w_id,
-			d_name, d_street1, d_street2, d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id)
+		v := fmt.Sprintf(`(%d, %d, '%s', '%s', '%s', '%s', '%s', '%s', %f, %f, %d)`, dID, dWID,
+			dName, dStreet1, dStreet2, dCity, dState, dZip, dTax, dYtd, dNextOID)
 
 		if err := l.InsertValue(ctx, v); err != nil {
 			return err
@@ -135,11 +135,61 @@ d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id) VALUES `, tableID)
 }
 
 func (w *Workloader) loadCustomer(ctx context.Context, tableID int, warehouse int, district int) error {
+	s := w.base.GetState(ctx)
 
-	return nil
+	hint := fmt.Sprintf(`INSERT INTO customer%d (c_id, c_d_id, c_w_id, c_last, c_middle, c_first, 
+c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_since, c_credit, c_redit_limt,
+c_discount, c_balance, c_ytd_payment, c_payment_cnt, c_delivery_cnt, c_data) VALUES `, tableID)
+
+	l := load.NewBatchLoader(s.Conn, hint)
+
+	for i := 0; i < customerPerDistrict; i++ {
+		s.Buf.Reset()
+
+		cID := i + 1
+		cDID := district
+		cWID := warehouse
+		var cLast string
+		if i < 1000 {
+			cLast = randCLastSyllables(i, s.Buf)
+		} else {
+			cLast = randCLast(s.R, s.Buf)
+		}
+		cMiddle := "OE"
+		cFirst := randChars(s.R, s.Buf, 8, 16)
+		cStreet1 := randChars(s.R, s.Buf, 10, 20)
+		cStreet2 := randChars(s.R, s.Buf, 10, 20)
+		cCity := randChars(s.R, s.Buf, 10, 20)
+		cState := randState(s.R, s.Buf)
+		cZip := randZip(s.R, s.Buf)
+		cPhone := randNumbers(s.R, s.Buf, 16, 16)
+		cSince := time.Now().Format("2006-01-02 15:04:05")
+		cCredit := "GC"
+		if s.R.Intn(10) == 0 {
+			cCredit = "BC"
+		}
+		cCreditLim := 50000.00
+		cDisCount := float64(randInt(s.R, 0, 5000)) / float64(10000.0)
+		cBalance := -10.00
+		cYtdPayment := 10.00
+		cPaymentCnt := 1
+		cDeliveryCnt := 0
+		cData := randChars(s.R, s.Buf, 300, 500)
+
+		v := fmt.Sprintf(`(%d, %d, %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %f, %f, %f, %f, %d, %d, '%s')`,
+			cID, cDID, cWID, cLast, cMiddle, cFirst, cStreet1, cStreet2, cCity, cState,
+			cZip, cPhone, cSince, cCredit, cCreditLim, cDisCount, cBalance,
+			cYtdPayment, cPaymentCnt, cDeliveryCnt, cData)
+		if err := l.InsertValue(ctx, v); err != nil {
+			return err
+		}
+	}
+
+	return l.Flush(ctx)
 }
 
 func (w *Workloader) loadHistory(ctx context.Context, tableID int, warehouse int, district int, customer int) error {
+
 	return nil
 }
 
--- tpcc/rand.go
+++ tpcc/rand.go
@@ -2,6 +2,7 @@ package tpcc
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/siddontang/go-tpc/pkg/util"
 )
@@ -74,4 +75,48 @@ func randOriginalString(r *rand.Rand, b *util.BufAllocator) string {
 	}
 
 	return randChars(r, b, 26, 50)
+}
+
+var (
+	cLoad       int
+	cCustomerID int
+	cItemID     int
+)
+
+var cLastTokens = [...]string{
+	"BAR", "OUGHT", "ABLE", "PRI", "PRES",
+	"ESE", "ANTI", "CALLY", "ATION", "EING"}
+
+func randCLastSyllables(n int, b *util.BufAllocator) string {
+	// 3 tokens * max len
+	buf := b.Alloc(3 * 5)
+	buf = buf[:0]
+	buf = append(buf, cLastTokens[n/100]...)
+	n = n % 100
+	buf = append(buf, cLastTokens[n/10]...)
+	n = n % 10
+	buf = append(buf, cLastTokens[n]...)
+	return util.String(buf)
+}
+
+func init() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	cLoad = r.Intn(256)
+	cItemID = r.Intn(1024)
+	cCustomerID = r.Intn(8192)
+}
+
+// refer 4.3.2.3 and 2.1.6
+func randCLast(r *rand.Rand, b *util.BufAllocator) string {
+	return randCLastSyllables(((r.Intn(256)|r.Intn(1000))+cLoad)%1000, b)
+}
+
+// refer 2.1.6
+func randCustomerID(r *rand.Rand) int {
+	return ((r.Intn(1024) | (r.Intn(3000) + 1) + cCustomerID) % 3000) + 1
+}
+
+// refer 2.1.6
+func randItemID(r *rand.Rand) int {
+	return ((r.Intn(8190) | (r.Intn(100000) + 1) + cItemID) % 100000) + 1
 }

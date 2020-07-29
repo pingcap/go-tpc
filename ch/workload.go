5cccb5f4085c6120290aebb4b3806e5e6ce59c3a
mahjonp <junpeng.man@gmail.com>
2020-07-29 16:48:45 +0800

    measure error of ch
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

--- ch/workload.go
+++ ch/workload.go
@@ -184,16 +184,11 @@ func (w Workloader) Run(ctx context.Context, threadID int) error {
 
 	start := time.Now()
 	rows, err := s.Conn.QueryContext(ctx, query)
+	w.measurement.Measure(queryName, time.Now().Sub(start), err)
 	if err != nil {
-		return fmt.Errorf("execute query %s failed %v", query, err)
+		return fmt.Errorf("%s execute query %s failed %v", time.Now().Format("2006-01-02 15:04:05"), queryName, err)
 	}
 	defer rows.Close()
-	w.measurement.Measure(queryName, time.Now().Sub(start), err)
-
-	if err != nil {
-		return fmt.Errorf("execute %s failed %v", queryName, err)
-	}
-
 	return nil
 }
 

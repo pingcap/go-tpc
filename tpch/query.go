97009c9b58e943e619879557d969a24b6567f937
Song Gao <disxiaofei@163.com>
2023-01-04 13:00:21 +0800

    fix-q15 (#159)

--- tpch/query.go
+++ tpch/query.go
@@ -402,18 +402,6 @@ where
 	and l_shipdate < date_add('1996-12-01', interval '1' month);
 `
 	q15 = `
-create view revenue0 (supplier_no, total_revenue) as
-	select
-		l_suppkey,
-		sum(l_extendedprice * (1 - l_discount))
-	from
-		lineitem
-	where
-		l_shipdate >= '1997-07-01'
-		and l_shipdate < date_add('1997-07-01', interval '3' month)
-	group by
-		l_suppkey;
-
 /*PLACEHOLDER*/ select
 	s_suppkey,
 	s_name,
@@ -433,8 +421,6 @@ where
 	)
 order by
 	s_suppkey;
-
-drop view revenue0;
 `
 	q16 = `
 /*PLACEHOLDER*/ select
